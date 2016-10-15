34540e668aa2075395abd27f9ad738c3e2a3e555
Christopher Davis <cdchris12@gmail.com>
2016-10-14 23:06:50 -0500

    Updated temperature reporting method to increase accuracy of reported temperature.

--- CherryPyServer.py
+++ CherryPyServer.py
@@ -6,6 +6,14 @@ import cherrypy
 from sense_hat import SenseHat
 import json
 
+def toCelcius(temp):
+    temp *= 9.0
+    temp /= 5.0
+    temp += 32.0
+    
+    return temp
+# End def
+
 # Instantiate only one SenseHat object, which we will route every request through 
 sense = SenseHat()
 
@@ -15,15 +23,13 @@ class SenseHatServer(object):
     def index(self):
         data = {}
         
-        # Convert from Celcius to Farenheit
-        temp = sense.get_temperature()
-        temp *= 9.0
-        temp /= 5.0
-        temp += 32.0 # Gotta use 32.0, because we want to add a float to a float
+        # Since the temperature sensor can be affected by the CPU heatsink heat dissipation, we're going to aggregate every temp we can get from the Pi, aggregate them together, and report back the average of the three
+        temp_aggregate = sense.get_temperature() + sense.get_temperature_from_pressure() + sense. get_temperature_from_humidity()
+        temp_aggregate /= 3
         
         # Populate return data structure
         data["humidity"] = sense.get_humidity()
-        data["temperature"] = temp
+        data["temperature"] = toCelcius( temp_aggregate )
         data["pressure"] = sense.get_pressure()
         
         return json.dumps(data)
